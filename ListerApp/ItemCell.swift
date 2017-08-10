9bebec412d3d1b0f929de8609764bc160e91653b
Ajisaputrars <ajisaputrars@gmail.com>
2017-08-10 09:34:29 +0700

    User Interface Done

---
+++ ListerApp/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  ListerApp
+//
+//  Created by Aji Saputra Raka Siwi on 8/10/17.
+//  Copyright © 2017 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
